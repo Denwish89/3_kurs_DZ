cebb383dae0f100d7fa75ca4223641b50140b681
denis <denwish@list.ru>
2023-03-21 07:26:47 +0300

    Выполнена задача 29

--- Lesson4/Task29/Program.cs
+++ Lesson4/Task29/Program.cs
@@ -2,3 +2,30 @@
 //заполненный псевдослучайными числами и выводит их на экран.
 // 1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19] 
 // 6, 1, 33 -> [6, 1, 33]
+
+int[] array = new int[8];
+Random rand = new Random();
+
+    Array(array);
+    WriteArray(array);
+    Console.Write("-> [");
+    WriteArray(array);
+    Console.Write("]");
+
+void Array(int[] arr)
+{
+    Random rand = new Random();
+    for (int i = 0; i < arr.Length; i++)
+    {
+        arr[i] = rand.Next(10, 100);
+    }
+}
+
+void WriteArray(int[] arr)
+{
+    for (int i = 0; i < arr.Length; i++)
+    {
+                Console.Write(arr[i] + " ");
+
+    }
+}
