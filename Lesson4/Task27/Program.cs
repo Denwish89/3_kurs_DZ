ee965efd871c8c99bdaefa90cb70c73dcfd889e0
denis <denwish@list.ru>
2023-03-21 07:08:58 +0300

    Выполнена задача 27

--- Lesson4/Task27/Program.cs
+++ Lesson4/Task27/Program.cs
@@ -2,3 +2,26 @@
 // 452 -> 11
 // 82 -> 10
 // 9012 -> 12
+
+// 1. Принять число
+// 2. Вывести сумму цифр в числе
+
+Console.Write("Введите натуральное число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int sumNumber = SumNumber(number);
+Console.WriteLine($"Сумма цифр числа {number} -> {sumNumber}");
+
+int SumNumber(int num)
+{
+    int ostNum = 0;
+    int sumNum = 0;
+    
+    while (num != 0)
+    {
+        sumNum = sumNum + num % 10;
+        num = num / 10;
+    }
+    sumNum = sumNum + num;
+    return sumNum;
+}
