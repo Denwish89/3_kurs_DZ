687b3048208f5b84fc34c73feddfc354b10f547d
denis <denwish@list.ru>
2023-03-30 14:43:37 +0300

    Выполнена Task50

--- Lesson7/Task50/Program.cs
+++ Lesson7/Task50/Program.cs
@@ -7,5 +7,51 @@
 // 8 4 2 4
 // 1, 7 -> такого элемента в массиве нет
 
+Console.Write("Укажите размеры массива по x: ");
+int xRows = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Укажите размеры массива по y: ");
+int yColumns = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Укажите x позицию элемента: ");
+int xCoordinate = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Укажите y позицию элемента: ");
+int yCoordinate = Convert.ToInt32(Console.ReadLine());
+
+if (xCoordinate > xRows || yCoordinate > yColumns)
+{
+    Console.WriteLine($"{xCoordinate}, {yCoordinate} -> такого элемента в массиве нет");
+}
+else
+{
+    int xCoordinateRows = xCoordinate - 1;
+    int yCoordinateColumns = yCoordinate - 1;
+
+    int number = Number(xRows, yColumns, 1, 10, xCoordinateRows, yCoordinateColumns);
+    Console.WriteLine($"{xCoordinate}, {yCoordinate} -> {number}");
+}
+
+int Number(int rows, int columns, int min, int max, int x, int y)
+{
+    int[,] matrix = new int[rows, columns];
+    int num = 0;
+
+    Random rnd = new Random();
+
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = rnd.Next(min, max + 1);
+        }
+    }
+
+    return num = matrix[x, y];
+}
+
+
+
+
 
 
