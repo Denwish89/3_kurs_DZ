2a00483bd6a021e83d0af884ec781ab60886879c
denis <denwish@list.ru>
2023-03-30 12:30:29 +0300

    Выполнена Task47

--- Lesson7/Task47/Program.cs
+++ Lesson7/Task47/Program.cs
@@ -7,4 +7,36 @@
 // 1 -3,3 8 -9,9
 // 8 7,8 -7,1 9
 
+double[,] CreateMatrixRndInt(int rows, int columns, int min, int max) // метод создающий двумерный массив через запятую [,]
+{
+    double[,] matrix = new double[rows, columns];
+
+    Random rnd = new Random();
+
+    for (int i = 0; i < matrix.GetLength(0); i++) //matrix.GetLength(0) - кол-во строк, "0" это индекс, означает строки
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++) //matrix.GetLength(1) - кол-во столбцов, "1" это индекс, означает столбцы
+        {
+            matrix[i, j] = Math.Round((rnd.NextDouble() * (max - min) + min), 1);
+        }
+    }
+    return matrix;
+}
+
+void PrintMatrix(double[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        Console.Write("[");
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.Write($"{matrix[i, j],4} ");
+        }
+        Console.WriteLine("]");
+    }
+
+}
+
+double[,] array2d = CreateMatrixRndInt(3, 4, -10, 10);
+PrintMatrix(array2d);
 
