78f3eda6a3ce5f1f8943b04a975bbabbd3c0c999
denis <denwish@list.ru>
2023-04-01 13:11:05 +0300

    Выполнена Task54

---
+++ Lesson8/Task54/Program.cs
@@ -0,0 +1,58 @@
+﻿// Задача 54. Задайте двумерный массив. Напишите программу, которая
+// упорядочит по убыванию элементы каждой сторки двумерного массива.
+
+int[,] array2d = CreateMatrixRndInt(3, 4, 1, 10);
+PrintMatrix(array2d);
+SelectionSort(array2d);
+Console.WriteLine();
+PrintMatrix(array2d);
+
+int[,] CreateMatrixRndInt(int rows, int columns, int min, int max)
+{
+    int[,] matrix = new int[rows, columns];
+
+    Random rnd = new Random();
+
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = rnd.Next(min, max + 1);
+        }
+    }
+    return matrix;
+}
+
+void PrintMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        Console.Write("[");
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.Write($"{matrix[i, j],3} ");
+        }
+        Console.WriteLine("]");
+    }
+
+}
+
+void SelectionSort(int[,] arraySort)
+{
+    for (int i = 0; i < arraySort.GetLength(0); i++)
+    {
+        for (int j = 0; j < arraySort.GetLength(1); j++)
+        {
+            for (int min = 0; min < arraySort.GetLength(1) - 1; min++)
+            {
+                if (arraySort[i, min] < arraySort[i, min + 1])
+                {
+                    int temporary = arraySort[i, min + 1];
+                    arraySort[i, min + 1] = arraySort[i, min];
+                    arraySort[i, min] = temporary;
+                }
+            }
+        }
+    }
+}
+
---
+++ Lesson8/Task56/Program.cs
@@ -0,0 +1,64 @@
+﻿// Задача 56. Задайте прямоугольный двумерный массив.
+// Напишите программу, которая будет находить строку 
+// с наименьшей суммой элементов.
+
+int[,] array2d = CreateMatrixRndInt(4, 3, 1, 10);
+PrintMatrix(array2d);
+int sum = SumRows(array2d, 0);
+int minSum = 1;
+int minSumRow = MinSumRow(array2d);
+Console.WriteLine();
+Console.WriteLine($"Наименьшая сумма элементовв строке: {minSum}");
+
+int[,] CreateMatrixRndInt(int rows, int columns, int min, int max)
+{
+    int[,] matrix = new int[rows, columns];
+
+    Random rnd = new Random();
+
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = rnd.Next(min, max + 1);
+        }
+    }
+    return matrix;
+}
+
+void PrintMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        Console.Write("[");
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.Write($"{matrix[i, j],3} ");
+        }
+        Console.WriteLine("]");
+    }
+
+}
+
+int SumRows(int[,] array, int i)
+{
+    int sum = array[i, 0];
+    for (int j = 1; j < array.GetLength(1); j++)
+    {
+        sum += array[i, j];
+    }
+    return sum;
+}
+
+int MinSumRow(int[,] array2d)
+{
+    for (int i = 1; i < array2d.GetLength(0); i++)
+    {
+        if (sum > SumRows(array2d, i))
+        {
+            sum = SumRows(array2d, i);
+            minSum = i + 1;
+        }
+    }
+    return minSum;
+}
